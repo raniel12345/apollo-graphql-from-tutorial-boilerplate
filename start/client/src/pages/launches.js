b29c3f2a234e047d7dc8e53821dea3b9ce0a97a3
Raniel Garcia <ranielgarcia2596@gmail.com>
2019-12-26 22:37:24 +0800

    Just want to push for my ref

--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -1,5 +1,82 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LaunchTile, Header, Button, Loading } from "../components";
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 export default function Launches() {
-  return <div />;
+  const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
+  console.log(data);
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR {error.message}</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map(launch => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+      {data.launches && data.launches.hasMore && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                after: data.launches.cursor
+              },
+              updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                console.log({
+                  ...fetchMoreResult
+                });
+                console.log(fetchMoreResult);
+                if (!fetchMoreResult) return prev;
+                return {
+                  ...fetchMoreResult,
+                  launches: {
+                    ...fetchMoreResult.launches,
+                    launches: [
+                      // Combine the previous results to fetchmore results
+                      ...prev.launches.launches,
+                      ...fetchMoreResult.launches.launches
+                    ]
+                  }
+                };
+              }
+            })
+          }
+        >
+          Load More
+        </Button>
+      )}
+    </Fragment>
+  );
 }
